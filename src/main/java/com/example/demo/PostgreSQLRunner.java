b12830ae351648f926a586aee1f8299dad9459d0
dhtlsdn <dhtlsdn@nsdevil.com>
2021-02-28 14:38:42 +0900

    금일 내용 수정

---
+++ src/main/java/com/example/demo/PostgreSQLRunner.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+
+@Component
+public class PostgreSQLRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(connection);
+            String URL = connection.getMetaData().getURL();
+            System.out.println(URL);
+            String User = connection.getMetaData().getUserName();
+            System.out.println(User);
+
+        }
+
+        jdbcTemplate.execute("SELECT  id, name from sample");
+        jdbcTemplate.execute("INSERT INTO public.sample (id, \"name\") VALUES('dhtlswo_5', '합체_5')");
+    }
+}
